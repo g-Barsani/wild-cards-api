cdd8eed551e7d602354a7156a6a2a99d28cf0a33
Gabriel Tavares Barsani <103264805+g-Barsani@users.noreply.github.com>
2024-10-15 16:17:23 -0300

    Initial commit

---
+++ wild-cards/wild-cards/src/main/java/com/meialuaquadrado/wild_cards/WildCardsApplication.java
@@ -0,0 +1,13 @@
+package com.meialuaquadrado.wild_cards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildCardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildCardsApplication.class, args);
+	}
+
+}
---
+++ wild-cards/wild-cards/src/test/java/com/meialuaquadrado/wild_cards/WildCardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meialuaquadrado.wild_cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildCardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
